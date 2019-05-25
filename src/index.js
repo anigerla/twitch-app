813798c4d651debf4150ca4da70c10785852cffa
Regina <regina.zaripova@gmail.com>
2019-05-25 14:51:23 -0400

    redux-form & form validation added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+// import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+// const composeEnhancers = window.___REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducers,
+    // composeEnhancers(applyMiddleware())
+    composeWithDevTools(applyMiddleware()),
+);
 
 ReactDOM.render(
     <Provider store={store}>
